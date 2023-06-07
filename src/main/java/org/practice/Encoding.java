07fe89492d7e9fb2057ac917b5e9e2cd24e516fa
purrym <wanjirapurity@gmail.com>
2023-06-07 15:24:40 +0300

    updated the app code 4 expected output of encoding

--- src/main/java/org/practice/Encoding.java
+++ src/main/java/org/practice/Encoding.java
@@ -1,4 +1,21 @@
 package org.practice;
 
 public class Encoding {
+    private static final String alphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    public static String encode(String msg, int key){
+        StringBuilder output = new StringBuilder();
+        for(char c: msg.toCharArray()){
+            int charPos = alphabets.indexOf(c);
+            if (charPos == -1){
+                output.append(c);
+                continue;
+            }
+            int tempNewPos = charPos + key;
+            int newPos = tempNewPos > 25 ?  tempNewPos % 26 : tempNewPos;
+            char replacement = alphabets.charAt(newPos);
+            output.append(replacement);
+        }
+        return output.toString();
+    }
 }
