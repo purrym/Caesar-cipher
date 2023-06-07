f27084a8866b08962d30f59b1e0b75da38c49e95
purrym <wanjirapurity@gmail.com>
2023-06-07 15:23:40 +0300

    updated the app code 4 expected output of decoding

--- src/main/java/org/practice/Decoding.java
+++ src/main/java/org/practice/Decoding.java
@@ -1,4 +1,20 @@
 package org.practice;
-
 public class Decoding {
+    private static final String alphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    public static String decode(String msg, int key){
+        StringBuilder output = new StringBuilder();
+        for(char c: msg.toCharArray()){
+            int charPos = alphabets.indexOf(c);
+            if (charPos == -1){
+                output.append(c);
+                continue;
+            }
+            int tempNewPos = charPos - key;
+            int newPos = tempNewPos < 0 ? 26 + tempNewPos : tempNewPos;
+            char replacement = alphabets.charAt(newPos);
+            output.append(replacement);
+        }
+        return output.toString();
+    }
 }
